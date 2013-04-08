dd065d10b8748e97088d47fc6e03c6b3a13cee80
xingmin liu <xingminliu@qq.com>
2013-04-08 12:19:41 +0800

    Fix the git's install shell script bugs.

--- msysgit/install_git.ps1
+++ msysgit/install_git.ps1
@@ -3,8 +3,8 @@ Write-Host "installing git for windows(msysgit)"
 $curpwd = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Set-Location -Path $curpwd
 
-& .\Git-1.8.1.2-preview20130201.exe /SP- /SILENT /DIR="C:\xingmin\git" /CLOSEAPPLICATIONS /NOCANCEL /NORESTART
-& C:\xingmin\git\bin\sh.exe --login -c "git config --global user.name xingmin liu && git config --global user.email xingminliu@qq.com"
+Start-Process .\\Git-1.8.1.2-preview20130201.exe -ArgumentList "/SP- /SILENT /DIR=C:\\xingmin\\git /CLOSEAPPLICATIONS /NOCANCEL /NORESTART" -Wait
+& C:\xingmin\git\bin\sh.exe --login -c "git config --global user.name 'xingmin liu' && git config --global user.email xingminliu@qq.com"
 
 
 Pop-Location
