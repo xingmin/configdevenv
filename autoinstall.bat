4a20896df07b21c30aefa18ba109a79a12f09452
xingmin liu <xingminliu@qq.com>
2013-04-08 18:40:29 +0800

    Make the install.ps1 in the runnig range

--- autoinstall.bat
+++ autoinstall.bat
@@ -4,8 +4,8 @@ IF ERRORLEVEL 0 goto ps
 IF NOT ERRORLEVEL 0 goto installps
 
 :installps
+ECHO "installing PowerShell"
 start /wait .\WindowsXP-KB968930-x86-CHS.exe
 
 :ps
-ECHO "test"
-rem powershell -command "& {.\install.ps1}"
+powershell -command "& {.\install.ps1}"
