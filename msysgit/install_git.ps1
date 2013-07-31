0192449aca39c5500c3b41b7e6235ee068fff727
xingmin liu <xingminliu@qq.com>
2013-07-31 17:29:05 +0800

    checking commands whether has been installed.

--- msysgit/install_git.ps1
+++ msysgit/install_git.ps1
@@ -1,10 +1,11 @@
 Push-Location
-Write-Host "installing git for windows(msysgit)"
+Write-Host "installing git for windows(msysgit)--"
 $curpwd = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Set-Location -Path $curpwd
-
-Start-Process .\\Git-1.8.1.2-preview20130201.exe -ArgumentList "/SP- /SILENT /DIR=C:\\xingmin\\git /CLOSEAPPLICATIONS /NOCANCEL /NORESTART" -Wait
-& C:\xingmin\git\bin\sh.exe --login -c "git config --global user.name 'xingmin liu' && git config --global user.email xingminliu@qq.com"
-
-
+if (get-command git -erroraction silentlycontinue){
+	Write-Host "Git has been installed, this setup do nothing."
+}else{
+	Start-Process .\\Git-1.8.1.2-preview20130201.exe -ArgumentList "/SP- /SILENT /DIR=C:\\xingmin\\git /CLOSEAPPLICATIONS /NOCANCEL /NORESTART" -Wait
+	& C:\xingmin\git\bin\sh.exe --login -c "git config --global user.name 'xingmin liu' && git config --global user.email xingminliu@qq.com"
+}
 Pop-Location
--- python3.3/install_python.ps1
+++ python3.3/install_python.ps1
@@ -3,16 +3,20 @@ $curpwd = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Set-Location -Path $curpwd
 
 Write-Host "install python-3.3"
-Start-Process msiexec -ArgumentList "/i python-3.3.0.msi /passive TARGETDIR=C:\\xingmin\\python3.3 ALLUSER=1 ADDLOCAL=ALL" -Wait
-Write-Host "write system environment of python"
-$oldenvpath = Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Session Manager\Environment" -Name Path
-$newenvpath = "C:\xingmin\python3.3;"+$oldenvpath.Path
-Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Session Manager\Environment" -Name Path -Value $newenvpath
-$env:Path = $env:Path + ";C:\xingmin\python3.3"
 
-Write-Host "install pywin32"
-Copy-Item -Force -Recurse .\pywin32-218.win32-py3.3\PLATLIB\* C:\xingmin\python3.3\Lib\site-packages
-Copy-Item -Force -Recurse .\pywin32-218.win32-py3.3\SCRIPTS\* C:\xingmin\python3.3\Lib\site-packages
-& python C:\xingmin\python3.3\Lib\site-packages\pywin32_postinstall.py -quiet -install
+if (get-command python -erroraction silentlycontinue){
+	Write-Host "python has been installed before and this setup do nothing."
+}else{
+	Start-Process msiexec -ArgumentList "/i python-3.3.0.msi /passive TARGETDIR=C:\\xingmin\\python3.3 ALLUSER=1 ADDLOCAL=ALL" -Wait
+	Write-Host "write system environment of python"
+	$oldenvpath = Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Session Manager\Environment" -Name Path
+	$newenvpath = "C:\xingmin\python3.3;"+$oldenvpath.Path
+	Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Session Manager\Environment" -Name Path -Value $newenvpath
+	$env:Path = $env:Path + ";C:\xingmin\python3.3"
 
+	Write-Host "install pywin32"
+	Copy-Item -Force -Recurse .\pywin32-218.win32-py3.3\PLATLIB\* C:\xingmin\python3.3\Lib\site-packages
+	Copy-Item -Force -Recurse .\pywin32-218.win32-py3.3\SCRIPTS\* C:\xingmin\python3.3\Lib\site-packages
+	& python C:\xingmin\python3.3\Lib\site-packages\pywin32_postinstall.py -quiet -install
+}
 Pop-Location
