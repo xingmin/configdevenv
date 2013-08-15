1d5ad4aedbdf8e9170043ee571df26569a5ff311
xingmin-yszxxy <xingmin@yszxyy.com>
2013-08-15 21:10:27 +0800

    delete some comments and make reg command output to nul

--- deltask.bat
+++ deltask.bat
@@ -3,5 +3,5 @@ if %ERRORLEVEL% EQU 0 (goto 0) else (goto EOF)
 :0
 @reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v selfinstalldevenv /f  >nul 2>&1 
 :EOF
-@echo "Finished!" 
+
 exit /b 0
--- selfinstall.bat
+++ selfinstall.bat
@@ -5,14 +5,14 @@ pushd %cd%
 cd /d %~dp0
 
 ECHO OFF
-reg export HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall uninstall.txt
+reg export HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall uninstall.txt >nul 2>&1
 for /f "usebackq tokens=2 delims=:" %%i in (`%windir%\system32\find.exe /i /c "KB968930" uninstall.txt`) do (if %%i GEQ 1 goto ps)
 
 
 :installps
 ECHO "installing PowerShell"
 start /wait .\WindowsXP-KB968930-x86-CHS.exe
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v selfinstalldevenv /t reg_sz /d "%~dp0selfinstall.bat" /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v selfinstalldevenv /t reg_sz /d "%~dp0selfinstall.bat" /f >nul 2>&1
 shutdown /r /f /t 0
 exit /b 1
 
