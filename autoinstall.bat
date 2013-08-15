6d5f15245737c21084c6d7a198cea1a7ea8d6ee7
xingmin-yszxxy <xingmin@yszxyy.com>
2013-08-15 14:40:37 +0800

    change the policy after installed the tools

--- autoinstall.bat
+++ autoinstall.bat
@@ -5,15 +5,20 @@ pushd %cd%
 cd /d %~dp0
 
 ECHO OFF
-PowerShell -Command "& {echo 'powershell has been installed. This time do nothing.'}"
-IF ERRORLEVEL 0 goto ps
-IF NOT ERRORLEVEL 0 goto installps
+reg export HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall uninstall.txt
+for /f "usebackq tokens=2 delims=:" %%i in (`find /i /c "KB968930" uninstall.txt`) do (if %%i GEQ 1 goto ps)
+
 
 :installps
 ECHO "installing PowerShell"
 start /wait .\WindowsXP-KB968930-x86-CHS.exe
 if not exist "reboot.txt" touch reboot.txt & echo "This file existed here, it shows this system needs reboot." > reboot.txt
+exit /b 1
+
 :ps
+if exist uninstall.txt del uninstall.txt
 powershell -command "& {.\install.ps1}"
 
 popd
+
+exit /b 0
